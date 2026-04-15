925de241ce926e394c4235e29a575e270011d02d
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-04-15 23:22:02 +0600

    Banner section design complete.

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import Root from "../pages/Root/Root";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Home from "../pages/Home/Home";
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +12,11 @@ export const router = createBrowserRouter([
       path: "*",
       element: <ErrorPage />,
     },
+    {
+      index: true,
+      path: "/",
+      Component: Home
+    }
     ]
   },
 ]);
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import heroimg from "../../assets/hero.png"
 const Banner = () => {
     return (
-        <div>
-
+        <div className='text-center pt-30 space-y-10 bg-base-300'>
+            <div>
+                <h1 className='text-7xl font-extrabold'>We Build <br /><span className='bg-gradient-to-r from-[#6b35e5] to-purple-500 bg-clip-text text-transparent'>Productive</span> Apps</h1>
+                <p className='text-xl text-gray-500'>At HERO.IO , we craft innovative apps designed to make everyday life simpler, smarter, and more exciting. <br /> Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
+            </div>
+            <div className='flex gap-5 justify-center items-center'>
+                <button className='btn'><img className='w-10' src="https://img.icons8.com/?size=100&id=L1ws9zn2uD01&format=png&color=000000" alt="" />Google Play</button>
+                <button className='btn'><img className='w-10' src="https://img.icons8.com/?size=100&id=4PbFeZOKAc61&format=png&color=000000" alt="" />App Store</button>
+            </div>
+            <div className='flex items-center justify-center'>
+                <img src={heroimg} alt="" />
+            </div>
         </div>
     );
 };
