ad3d4a20eefe065283a6191ec0cbf2d5ececf2f4
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-04-15 23:02:21 +0600

    added some adjustment on errorpage and footer.

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -6,13 +6,13 @@ const ErrorPage = () => {
         navigate(-1);
     }
     return (
-        <div className="flex flex-col justify-center items-center space-y-10 mb-10">
+        <div className="flex flex-col justify-center items-center space-y-10 mb-10 pt-10">
             <div>
                 <img src={errorimage} alt="" />
             </div>
             <div className="text-center space-y-3">
                 <h1 className="text-4xl font-bold">Oops!Page Not Found!</h1>
-                <p>The page your are looking for is not available.</p>
+                <p className="text-gray-500">The page your are looking for is not available.</p>
                 <button onClick={handleGoBack} className="btn bg-gradient-to-r from-[#6b35e5] to-purple-400 text-white">Go Back</button>
             </div>
         </div>
