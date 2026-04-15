073551bc16162ab5ac427d47ab08fac52520ea4a
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-04-15 22:29:34 +0600

    NavBar design complete.

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../pages/Root/Root";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    
+  },
+]);
---
+++ src/components/Header/NavBar.jsx
@@ -0,0 +1,48 @@
+import { NavLink } from "react-router";
+import herologo from "../../assets/logo.png"
+import { FaGithub } from "react-icons/fa";
+
+const NavBar = () => {
+    const navLinkStyles = ({ isActive }) =>
+        `transition-all duration-200 hover:text-[#6b35e5] hover:underline hover:underline-offset-4 ${isActive ? "text-[#6b35e5] underline-2 underline-offset-4 font-bold" : "text-base-content"
+        }`;
+
+    return (
+        <div className="navbar bg-base-100 md:px-20">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex="-1"
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-40 p-5 shadow">
+                        <NavLink><span className="md:btn">Home</span></NavLink>
+                        <NavLink><span className="md:btn">Apps</span></NavLink>
+                        <NavLink><span className="md:btn">Installation</span></NavLink>
+                    </ul>
+                </div>
+                <div className="flex justify-center items-center gap-2">
+                    <img className="w-10" src={herologo} alt="Hero IO" />
+                    {/* 2. Applied Gradient Text Here */}
+                    <a className="text-xl md:text-3xl font-extrabold bg-gradient-to-r from-[#6b35e5] to-purple-400 bg-clip-text text-transparent cursor-pointer">
+                        Hero IO
+                    </a>
+                </div>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 flex gap-10 font-medium text-lg">
+                    {/* Added 'to' props and applied the dynamic styles */}
+                    <NavLink to="/" className={navLinkStyles}>Home</NavLink>
+                    <NavLink to="/apps" className={navLinkStyles}>Apps</NavLink>
+                    <NavLink to="/installation" className={navLinkStyles}>Installation</NavLink>
+                </ul>
+            </div>
+            <div className="navbar-end md:flex md:gap-2">
+                <button className="btn bg-green-400 text-white bg-gradient-to-r from-[#6b35e5] to-purple-400"><FaGithub />Sign In</button>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
